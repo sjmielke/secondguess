46e95ae4845caa91729a4d7453d8d640c76c7085
Sebastian J. Mielke <sjm@sjmielke.com>
2016-06-24 14:11:51 -0700

    Cleaner human-algo-counting. Looks good!

--- guess_matching.py
+++ guess_matching.py
@@ -1,27 +1,13 @@
 from difflib import SequenceMatcher
 from collections import defaultdict
 from typing import Dict, List
-import sys
 
 from guess_helper import load_file_lines
 
+# mono2affix_min5_match3
 def is_legal_match(w1: str, w2: str, i1: int, i2: int, l: int) -> bool:
-  justsmallaffix = lambda a,b: (a == 0 or b == 0) and a + b <= 2
-  w1_ok = l / len(w1) > 0.9 or justsmallaffix(i1, len(w1) - (i1 + l)) and l >= 5
-  w2_ok = l / len(w2) > 0.9 or justsmallaffix(i2, len(w2) - (i2 + l)) and l >= 5
-  if sys.argv[1] == "mono2affix_min5":
-    return w1_ok
-  elif sys.argv[1] == "anyaffix":
-    return True
-  elif sys.argv[1] == "noaffix":
-    return l == len(w1)
-  elif sys.argv[1] == "mono2affix_min5_both":
-    return w1_ok and w2_ok
-  elif sys.argv[1] == "mono2affix_min5_match3":
-    return w1_ok and l >= 3
-  else:
-    print(sys.argv[1] + "is no real suffix")
-    exit(1)
+  i2_ = len(w1) - (i1 + l)
+  return l >= 3 and (l / len(w1) > 0.9 or (i1 == 0 or i2_ == 0) and i1 + i2_ <= 2 and l >= 5)
 
 def load_dictionary(path: str) -> (Dict[str, SequenceMatcher], Dict[str, List[str]]):
   matchers = {}
@@ -32,3 +18,84 @@ def load_dictionary(path: str) -> (Dict[str, SequenceMatcher], Dict[str, List[st
     translations[w].append(t)
   #print("{} distinct dictionary words to compare against loaded.".format(len(matchers.keys())))
   return (matchers, translations)
+
+def get_best_match(oov: str, lexword: str, matcher: SequenceMatcher) -> (str, (int, int, int)):
+  matcher.set_seq1(oov)
+  i_o, i_w, matchlength = matcher.find_longest_match(0, len(oov), 0, len(lexword))
+  return (lexword, (i_o, i_w, matchlength))
+
+def guess_actual_oov(ind: (int, int), oov: str, matchers: Dict[str, SequenceMatcher], translations: Dict[str, List[str]], cheat_guesses: Dict[str, str]) -> (str, str):
+  # Compare performance
+  what_the_algo_said = None
+  # Return
+  result = None
+  
+  # Match search
+  best_lexcandidates = []
+  best_matchlength = 0
+  found_legal = False
+  
+  # First find LCS = match with lexword in a mapreduce fashion
+  all_pairs = map(lambda w: (oov, w, matchers[w]), sorted(matchers.keys()))
+  
+  individual_bestmatches = itertools.starmap(get_best_match, all_pairs)
+  
+  # Now compare all findings!
+  #print(" ")
+  #print("?")
+  for (lexword, (i_o, i_w, matchlength)) in individual_bestmatches:
+    if found_legal and (matchlength < 3 or matchlength < best_matchlength):
+      continue
+    legal = guess_matching.is_legal_match(lexword, oov, i_w, i_o, matchlength)
+    if not found_legal or legal: # no legality regressions!
+      if matchlength == best_matchlength: # no improvement? just add a candidate
+        best_lexcandidates.append((lexword, i_w, i_o))
+      elif (legal and not found_legal # we find our first legal match...
+          or matchlength >= best_matchlength): # ...or a better one, regardless of legality status
+        best_lexcandidates = [(lexword, i_w, i_o)]
+        best_matchlength = matchlength
+    if legal:
+      found_legal = True
+  #print("!")
+  #print(" ")
+  
+  # Okay! Matching done, now choose something nice!
+  if best_matchlength == 0:
+    """
+    print("({:3}/{:3}) {:<20} ~~> none found!         {}".format(
+      ind[0]+1, ind[1]+1,
+      oov,
+      '=' if cheat_guesses[oov] == oov else ' '))
+    """
+    what_the_algo_said = oov
+    result = oov
+  else:
+    candidates = sorted(best_lexcandidates, key=lambda tup: len(tup[0])) # prefer shorter lexwords
+    
+    # First, what would the algo itself say?
+    what_the_algo_said = min(sorted(translations[candidates[0][0]], key=len)) if found_legal else oov
+    (lexword, lexindex, oovindex) = candidates[0]
+    result = what_the_algo_said
+    
+    # Now, can I find the "correct" solution? Then replace old result with it!
+    foundcheat = False
+    for (lw, li, oi) in candidates:
+      if cheat_guesses[oov] in translations[lw]:
+        (lexword, lexindex, oovindex) = (lw, li, oi)
+        result = cheat_guesses[oov]
+        foundcheat = True
+        break
+    
+    """
+    # Output individual results
+    print("({:3}/{:3}) {:<36}{:<36} {} {} {}   {:<20}".format( # 20 + 4 * 4 = 36
+      ind[0]+1, ind[1]+1,
+      guess_helper.bold(oov    , oovindex, best_matchlength),
+      guess_helper.bold(lexword, lexindex, best_matchlength),
+      '✔' if found_legal else '✗',
+      '❗' if foundcheat else ' ',
+      '=' if what_the_algo_said == cheat_guesses[oov] else ' ',
+      result if result != oov else ""))
+    """
+  
+  return (result, what_the_algo_said == cheat_guesses[oov])
--- thirdeye.py
+++ thirdeye.py
@@ -1,25 +1,34 @@
-import sys
+import itertools
 
 import guess_helper
 import guess_matching
 import guess_nes
 import guess_logic
 
-if __name__ == '__main__':
+def dict_only(oov_original_list_file: str, oov_cheat_list_file: str, datadir: str):
+  (_, translations) = guess_matching.load_dictionary(datadir + "lexicon.norm")
+  oov_original_list = guess_helper.load_file_lines(datadir + oov_original_list_file)
+  oov_cheat_list    = guess_helper.load_file_lines(datadir + oov_cheat_list_file)
+  
+  all_translations = set(itertools.chain(*translations.values()))
+  
+  with open(datadir + oov_cheat_list_file + "_dictonly", 'w') as outfile:
+    print("Writing to " + datadir + oov_cheat_list_file + "_dictonly")
+    for (oov, trans) in zip(oov_original_list, oov_cheat_list):
+      print(trans if trans in all_translations else oov, file=outfile)
 
-  datadir = "/home/sjm/documents/ISI/pyguess/data/"
-
+def doit(oov_original_list_file: str, datadir: str, cheatfile: str):
   # Load a bunch of things
+  oov_original_list = guess_helper.load_file_lines(datadir + oov_original_list_file)
   (matchers, translations) = guess_matching.load_dictionary(datadir + "lexicon.norm")
   nes = guess_helper.load_file_lines(datadir + "mudeval.unique_nes.r1") # apparently untokenized! sucks e.g. for Mosoni-Dunaig
-  oov_original_list = guess_helper.load_file_lines(datadir + "mud.oovlist")
-  cheat_guesses = dict(zip(oov_original_list, guess_helper.load_file_lines(datadir + "mud.oovlist.trans_sw_uniq_human_dictonly")))
+  cheat_guesses = dict(zip(oov_original_list, guess_helper.load_file_lines(datadir + oov_original_list_file + ".trans_sw_uniq_" + cheatfile)))
 
   # Start filling our guess dictionary!
   oov_guesses = {}
 
   (guessable_nes, guessable_oovs) = guess_logic.get_guessables_into(oov_guesses, oov_original_list, nes)
-  print("{} distinct NEs and {} distinct OOVs to guess.\n".format(len(guessable_nes), len(guessable_oovs)))
+  #print("{} distinct NEs and {} distinct OOVs to guess.\n".format(len(guessable_nes), len(guessable_oovs)))
 
   # Guess NEs
   all_ne_roots = guess_nes.guess_nes_into(oov_guesses, guessable_nes)
@@ -27,30 +36,35 @@ if __name__ == '__main__':
   # Then do the actual OOV guessing, while counting,
   # how often were we "better" than the human?
   # (Schema: count_human_thirdeye)
-  ((c_nn, c_ny), (c_yn, c_yw, c_yy)) = guess_logic.guess_actual_oovs_into(oov_guesses, guessable_oovs, matchers, translations, cheat_guesses)
+  ((c_nn, c_ny), (c_yn, c_yw, c_yc, c_yy)) = guess_logic.guess_actual_oovs_into(oov_guesses, guessable_oovs, matchers, translations, cheat_guesses)
 
   # Statistics
-  print("{}) {:<25} {:3} ({})".format(sys.argv[1],\
-    "No cheat, no algo:", c_nn, "neither knew a translation"))
-  print("{}) {:<25} {:3} ({})".format(sys.argv[1],\
-    "No cheat, but algo:", c_ny, "we're either superhuman or translated stray NEs"))
-  print("{}) {:<25} {:3} ({})".format(sys.argv[1],\
-    "Cheat, but no algo:", c_yn, "didn't find any candidate, but human did"))
-  print("{}) {:<25} {:3} ({})".format(sys.argv[1],\
-    "Cheat, but wrong algo:", c_yw, "didn't find the human suggestion containing candidate, but found something else"))
-  print("{}) {:<25} {:3} ({})".format(sys.argv[1],\
-    "Cheat = algo:", c_yy, "found the human suggestion containing candidate!"))
+  print("Comparing against the human in " + cheatfile)
+  print("> {:<27} {:3} ({})".format("No human, no algo:", \
+    c_nn, "neither knew a translation"))
+  print("> {:<27} {:3} ({})".format("No human, but algo:", \
+    c_ny, "we're either superhuman or translated stray NEs"))
+  print("> {:<27} {:3} ({})".format("Human, but no algo:", \
+    c_yn, "didn't find any translation, but human did"))
+  print("> {:<27} {:3} ({})".format("Human, but wrong algo:", \
+    c_yw, "didn't find the human translation containing lexicon entry, but found another"))
+  print("> {:<27} {:3} ({})".format("Human corrected algo:", \
+    c_yc, "found the human suggestion containing lexicon entry, but wouldn't have chosen it"))
+  print("> {:<27} {:3} ({})".format("Human = algo:", \
+    c_yy, "found the human suggestion containing candidate and naturally chose it!"))
 
   # Write our results in original order
-  with open(datadir + "mud.oovlist.trans_" + sys.argv[1], 'w') as translist:
+  with open(datadir + oov_original_list_file + ".trans_thirdeye_against_sw_uniq_" + cheatfile, 'w') as translist:
     for oov in oov_original_list:
       print(oov_guesses[oov], file=translist)
 
+if __name__ == '__main__':
+  datadir = "/home/sjm/documents/ISI/pyguess/data/"
+  oovfile = "mud.oovlist"
 
-  """
-    2
-  111
-    0
-  121
-   81
-  """
+  #dict_only(oovfile, "mud.oovlist.trans_sw_uniq_human", datadir)
+  #dict_only(oovfile, "mud.oovlist.trans_sw_uniq_reference", datadir)
+
+  for reference in ["human_dictonly", "human", "reference_dictonly", "reference"]:
+    doit(oovfile, datadir, reference)
+  
