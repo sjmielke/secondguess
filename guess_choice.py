1e4cf35657ffded710e168a7392695352e191730
Sebastian J. Mielke <sjm@sjmielke.com>
2016-07-05 16:28:27 -0700

    More cleanup

--- guess_choice.py
+++ guess_choice.py
@@ -1,78 +1,91 @@
+import itertools
+
 import guess_helper
 
 import sys
 
-def score_phrase(cand: "[CandidateWord]") -> float:
-	# each illegal result results in penalty
-	nomatch_penalty = 0.07 * sum(map(lambda w: 0 if w.islegal else 1, cand))
-	# prefer more OOV coverage (sum of matchlength [not translating is a full match!] by total oov length)
-	coverage = 0.15 * sum(map(lambda w: w.matchlength, cand)) / sum(map(lambda w: len(w.oov), cand))
-	# prefer shorter lexwords (0.05 is arbitrary)
-	lexlengths_penalty = 0.02 * sum(map(lambda w: len(w.lexword), cand))
-	# We want to minimize this!
-	scores = (float(sys.argv[-3]) * -1 * nomatch_penalty,
-	          float(sys.argv[-2]) *      coverage,
-	          float(sys.argv[-1]) * -1 * lexlengths_penalty)
-	return scores
-
-def choose_full_phrase_translation(unsorted_candidates: "[[CandidateWord]]", translations: "{str: [str]}", cheat_guesses: "{str: str}") -> (str, "Tuple[float]", bool):
-	# Compare performance
-	what_the_algo_said = None
-	# Return
-	result = None
+def choose_full_phrase_translation(
+		unsorted_candidates: "[[CandidateWord]]",
+		translations: "{str: [str]}",
+		cheat_guesses: "{str: str}",
+		debug_print: bool = False
+	) -> (str, "Tuple[float]", bool):
+	
+	#### HELPER FUNCTIONS
+	
+	def translate_candidate(candidate: "[CandidateWord]") -> "[[(CandidateWord, str)]]":		
+		translationss = []
+		for cw in candidate:
+			translationss.append([(cw, t) for t in translations[cw.lexword]] if cw.islegal else [(cw, cw.oov)])
+		return list(itertools.product(*translationss))
+	
+	def get_trans(transcandidate: "[(CandidateWord, str)]") -> str: return " ".join(map(lambda c: c[1], transcandidate))
+	
+	def score_phrase(cand: "[(CandidateWord, str)]") -> float:
+		cws = list(guess_helper.mapfst(cand))
+		# each illegal result results in penalty
+		nomatch_penalty = 0.07 * sum(map(lambda w: 0 if w.islegal else 1, cws))
+		# prefer more OOV coverage (sum of matchlength [not translating is a full match!] by total oov length)
+		coverage = 0.15 * sum(map(lambda w: w.matchlength, cws)) / sum(map(lambda w: len(w.oov), cws))
+		# prefer shorter lexwords (0.05 is arbitrary)
+		lexlengths_penalty = 0.02 * sum(map(lambda w: len(w.lexword), cws))
+		
+		# New features:
+		# reward new english words (justified OOVs)
+		# -> need the training target
+		# reward english in-domain words
+		# -> need domain-word-list
+		# reward hungarian words that also are OOVs
+		
+		# Combine the following two:?
+		# reward more common english words
+		# -> need some huge unigram model
+		# reward high-LM and punish low-LM phrases
+		# -> need a good english LM
+		
+		return (float(sys.argv[-3]) * -1 * nomatch_penalty,
+		        float(sys.argv[-2]) *      coverage,
+		        float(sys.argv[-1]) * -1 * lexlengths_penalty)
+	
+	#### ACTUAL CHOICE PROCESS
 	
 	# Now, first we have to evaluate the candidates into translation candidates!
-	
-	candidates = sorted(list(unsorted_candidates), key = score_phrase, reverse = True)
-	
-	phrase = list(guess_helper.mapfst(candidates[0]))
+	translated_unsorted_candidates = list(itertools.chain(*list(map(translate_candidate, unsorted_candidates))))
+	# Then pick our favorite one!
+	besttranscand = max(translated_unsorted_candidates, key = score_phrase)
+	phrase = list(map(lambda cw: cw.oov, guess_helper.mapfst(besttranscand)))
 	fullphrase = "".join(phrase)
 	
-	
 	# First, what would the algo itself say?
-	result_candidate = candidates[0] # translate "best" candidate
-	result_transwords = []
-	
-	algo_transwords = []
-	for (oov, lexword, _, _, _, legal) in result_candidate:
-		algo_transwords.append(min(translations[lexword], key=len) if legal else oov)
-	result_transwords = algo_transwords
-	what_the_algo_said = " ".join(algo_transwords)
-	result = what_the_algo_said
+	what_the_algo_said = get_trans(besttranscand)
+	result_transcandidate = besttranscand
 	
 	# Now, can I find the "correct" solution? Then replace old result with it!
 	foundcheat = False
-	cheatsolution = cheat_guesses[fullphrase].split()
-	if len(cheatsolution) == len(algo_transwords): # only if we have a chance
-		for candidate in candidates:
+	cheatsolution = cheat_guesses[fullphrase]
+	for transcandidate in translated_unsorted_candidates:
+		if get_trans(transcandidate) == cheatsolution:
 			foundcheat = True
-			for ((_, lexword, _, _, _, _), cheatword) in zip(candidate, cheatsolution):
-				if cheatword not in translations[lexword]:
-					foundcheat = False
-					break
-			if foundcheat:
-				result_candidate = candidate
-				result_transwords = cheatsolution
-				result = cheat_guesses[fullphrase]
-				break
+			result_transcandidate = transcandidate
+			break
 	
-	"""
-	# TODO prohibit inter-thread-foo
-	print(" » {} {}".format(
-		'❗' if foundcheat else ' ',
-		'=' if what_the_algo_said == cheat_guesses[fullphrase] else ' '))
-	for ((oov, lexword, lexindex, oovindex, matchlength, legal), trans) in zip(result_candidate, result_transwords):
-		if legal:
-			print("     {:<36} ✔ {:<36} -> {:<20}".format( # 20 + 4 * 4 = 36
-				guess_helper.bold(oov    , oovindex, matchlength),
-				guess_helper.bold(lexword, lexindex, matchlength),
-				trans))
-		else:
-			print("     {:<36} ✗ {:<20} -> {:<20}".format(
-				guess_helper.bold(oov    , oovindex, matchlength),
-				"---",
-				oov))
-	#"""
+	if debug_print:
+		# TODO prohibit inter-thread-foo
+		print(" » {} {}".format(
+			'❗' if foundcheat else ' ',
+			'=' if what_the_algo_said == cheat_guesses[fullphrase] else ' '))
+		for ((oov, lexword, lexindex, oovindex, matchlength, legal), trans) in zip(result_candidate, result_transwords):
+			if legal:
+				print("     {:<36} ✔ {:<36} -> {:<20}".format( # 20 + 4 * 4 = 36
+					guess_helper.bold(oov    , oovindex, matchlength),
+					guess_helper.bold(lexword, lexindex, matchlength),
+					trans))
+			else:
+				print("     {:<36} ✗ {:<20} -> {:<20}".format(
+					guess_helper.bold(oov    , oovindex, matchlength),
+					"---",
+					oov))
 	
-	return (result, score_phrase(result_candidate), what_the_algo_said == cheat_guesses[fullphrase])
-
+	return (get_trans(result_transcandidate),
+	        score_phrase(result_transcandidate),
+	        what_the_algo_said == cheat_guesses[fullphrase])
--- guess_phrases.py
+++ guess_phrases.py
@@ -1,22 +1,28 @@
 import itertools
 import operator # mul, itemgetter
 from functools import reduce
-from difflib import SequenceMatcher
 
 from guess_helper import mapfst
-import guess_matching
 import guess_choice
 
 def gen_phrases(segments: "[(str, str)]") -> "[[str]]":
 	segs_texts = list(mapfst(segments))
 	for sl in itertools.product(*([["", " "]] * (len(segs_texts) - 1))):
-		#print("gen {}".format(list(itertools.chain(*zip(segs_texts, sl))) + [segs_texts[-1]]))
 		yield ("".join(list(itertools.chain(*zip(segs_texts, sl))) + [segs_texts[-1]])).split()
 		# break # only full
 
-def phraseguess_actual_oov(oov: str, all_matches: "{str: [CandidateWord]}", translations: "{str: [str]}", catmorfdict: "{str: [(str, str)]}", cheat_guesses: "{str: str}") -> (str, float, bool):
+def phraseguess_actual_oov(
+		oov: str,
+		all_matches: "{str: [CandidateWord]}",
+		translations: "{str: [str]}",
+		catmorfdict: "{str: [(str, str)]}",
+		cheat_guesses: "{str: str}",
+		debug_print: bool = False,
+	) -> (str, float, bool):
+	
 	all_translations = []
-	#print("\n")
+	if debug_print:
+		print("\n")
 	for phrase in gen_phrases(catmorfdict[oov]):
 		candidatess = []
 		for phrase_segment in phrase:
@@ -24,11 +30,12 @@ def phraseguess_actual_oov(oov: str, all_matches: "{str: [CandidateWord]}", tran
 		
 		lengths = list(map(len, candidatess))
 		statstring = " x ".join(map(str, lengths)) + " = {}".format(reduce(operator.mul, lengths, 1))
-		#print (" » {:<20} » {:<20}".format(" ".join(phrase), statstring), end='', flush=True)
+		if debug_print:
+			print (" » {:<20} » {:<20}".format(" ".join(phrase), statstring), end='', flush=True)
 		
 		all_candidates = itertools.product(*candidatess)
 		
-		all_translations.append(guess_choice.choose_full_phrase_translation(all_candidates, translations, cheat_guesses))
+		all_translations.append(guess_choice.choose_full_phrase_translation(all_candidates, translations, cheat_guesses, debug_print))
 	
 	# Return best translation!
 	res = max(all_translations, key = lambda x: sum(x[1]))
@@ -38,6 +45,7 @@ def phraseguess_actual_oov(oov: str, all_matches: "{str: [CandidateWord]}", tran
 		if t == cheat_guesses[oov]:
 			res = (t, score, aeh)
 	
-	#print(" « {} {:<30}".format('=' if res[1] else ' ', res[0]))
+	if debug_print:
+		print(" « {} {:<30}".format('=' if res[1] else ' ', res[0]))
 	
 	return res
